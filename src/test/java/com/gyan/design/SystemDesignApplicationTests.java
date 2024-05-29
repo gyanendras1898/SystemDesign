51f094b04f0c78c4fb0032311c6dcba7075b7ba7
Gyanendra <gyanendras1898@gmail.com>
2024-05-30 00:03:25 +0530

    first commit

---
+++ src/test/java/com/gyan/design/SystemDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyan.design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
